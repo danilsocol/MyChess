15656ab0c5636593ad405954a5e06cb0406d7f67
danilsocol <danilsocol-2002@mail.ru>
2026-03-29 01:03:08 +0500

    fix DirectionalChessFigur e

--- ChessGame.Core/Models/Figures/Abstracts/DirectionalChessFigure.cs
+++ ChessGame.Core/Models/Figures/Abstracts/DirectionalChessFigure.cs
@@ -32,8 +32,8 @@ public abstract class DirectionalChessFigure : ChessFigure
                 
                 moves.Add(new PossibleMove(cell.Coordinate));
                 
-                columnPos = fromCoord.Column + direction.directionColumn;
-                linePos = fromCoord.Line + direction.directionLine;
+                columnPos =+ direction.directionColumn;
+                linePos =+ direction.directionLine;
             }
         }
 
